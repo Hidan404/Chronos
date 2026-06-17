5f3f7fb543ab90245ff1090c54f2876f8a659efe
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-06-16 22:36:01 -0300

    feat: restructure application by removing App component and adding Titulo with styling

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import "./styles/theme.css";
+import "./styles/global.css";
+
+import { Heading } from "./components/Heading";
+
+function Titulo() {
+  const nome = "Ronald";
+
+  return (
+    <div>
+      <Heading />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga unde
+        nostrum itaque. Ipsa illum sapiente accusamus iusto, reiciendis
+        repellendus sunt saepe exercitationem animi enim, error, possimus
+        explicabo quis unde? Officia?
+      </p>
+    </div>
+  );
+}
+
+export { Titulo };
---
+++ src/components/Heading.jsx
@@ -0,0 +1,8 @@
+import style from './Heading.module.css'
+
+
+export function Heading(){
+    return (
+        <h1 className={style.titulo}>Ola react</h1>
+    )
+}
