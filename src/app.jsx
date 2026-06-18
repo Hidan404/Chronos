df67fd796208e801961176dfc8fac4edfbd90b4b
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-06-17 22:53:23 -0300

    feat: refactor Titulo component layout and update global styles

--- src/app.jsx
+++ src/app.jsx
@@ -2,21 +2,34 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { Heading } from "./components/Heading.tsx";
-import { Timer } from "lucide-react"; 
+import { Timer } from "lucide-react";
 
 function Titulo() {
-  const nome = "Ronald";
-
   return (
-    <div>
-      <Heading >Ronald MAgnânimo<button><Timer/></button></Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga unde
-        nostrum itaque. Ipsa illum sapiente accusamus iusto, reiciendis
-        repellendus sunt saepe exercitationem animi enim, error, possimus
-        explicabo quis unde? Officia?
-      </p>
-    </div>
+    <>
+      <div className="container-fluid">
+        <div className="container">
+          <div className="content">
+            <p>Timer</p>
+          </div>
+        </div>
+        <div className="container">
+          <div className="content">
+            <p>Home</p>
+          </div>
+        </div>
+        <div className="container">
+          <div className="content">
+            <p>About</p>
+          </div>
+        </div>
+        <div className="container">
+          <div className="content">
+            <p>contato</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
